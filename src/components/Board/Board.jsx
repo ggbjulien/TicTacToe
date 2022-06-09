2fc98b87eb685a7fff55d239f160f9bbed8bf84f
ggbjulien <goetghebuer.julien@gmail.com>
2022-06-09 10:02:12 +0200

    Add functions to check if player has won

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import {
+  checkRow,
+  checkColumn,
+  checkDiagonal,
+} from "../../helpers/checkWinner";
 
 import styles from "./Board.module.css";
 
@@ -13,6 +18,27 @@ const Board = ({
   const [board, setBoard] = useState(
     [...Array(3)].map(() => Array(3).fill(null))
   );
+
+  const checkWinner = (row, col) => {
+    if (
+      checkRow(board, row) ||
+      checkColumn(board, col) ||
+      checkDiagonal(board)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const checkDraw = () => {
+    if (board.flat().every((cell) => cell)) {
+      updateWinner("draw");
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return <div className={styles.Board}>ROW</div>;
 };
